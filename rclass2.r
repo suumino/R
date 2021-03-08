609d2cc7437d1b7d9f542c325f6d8c95cf650051
suumino <78770211+suumino@users.noreply.github.com>
2021-03-08 17:17:51 +0900

    Add files via upload

---
+++ rclass2.r
@@ -0,0 +1,41 @@
+install.packages("dplyr")
+
+library(dplyr)
+
+summarise(iris, avg = mean(Sepal.Length))
+
+x <- c(1, 2, 3, 4)
+y <- c(1, 2)
+
+z <- x + yz
+
+z <- x * y
+z
+
+y <- c(1, 2)
+10 - y
+c(true, true) & c(true, true)
+
+True & True -> True
+True & False -> False
+False & True -> False  #
+False & False -> False  #
+
+False && True -> False  #
+False && False -> False  #
+
+
+True | True -> True
+True | False -> True
+False | True -> True  #  
+False | False -> False   #
+
+True || False -> True  #  
+False || False -> False   #
+ 
+
+ x <- c("a", "b")
+ "a" %in% x
+
+ x <- read.csv("reservation_r_csv.csv")
+ head(x)
